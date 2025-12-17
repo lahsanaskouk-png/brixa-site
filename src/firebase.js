26330a0db772ea9d71aaf93b879824014f28546b
lahsanaskouk-png <lahsanaskouk@gmail.com>
2025-12-18 00:05:48 +0100

    Initialize Firebase app and set up auth and Firestore

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmOOERF0KkDQ7G08L7_F7TuM7SItkoO_o",
+  authDomain: "brtiol.firebaseapp.com",
+  projectId: "brtiol",
+  storageBucket: "brtiol.firebasestorage.app",
+  messagingSenderId: "711908351848",
+  appId: "1:711908351848:web:a6c1ac4836988c4a7340dd"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
