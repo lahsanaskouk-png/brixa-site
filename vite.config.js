56fb5a413f26c1ea87638e50d964a0709f488745
lahsanaskouk-png <lahsanaskouk@gmail.com>
2025-12-17 23:36:34 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
+          ui: ['@headlessui/react', 'react-hot-toast']
+        }
+      }
+    }
+  },
+  css: {
+    devSourcemap: false
+  }
+})
